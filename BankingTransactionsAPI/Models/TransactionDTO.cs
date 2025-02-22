52754c95d0820b3058dcdc729d12f980039caa64
dvirbenshlush <dvirbsh95@gmail.com>
2025-02-22 15:09:53 +0200

    initiall db

---
+++ BankingTransactionsAPI/Models/TransactionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BankingTransactionsAPI.Models
+{
+    public class TransactionDTO
+    {
+        public decimal Amount { get; set; }
+        public string AccountNumber { get; set; }
+    }
+}
--- BankingTransactionsAPI/Program.cs
+++ BankingTransactionsAPI/Program.cs
@@ -1,6 +1,17 @@
+using BankingTransactionsAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient();
+
+// Add DbContext
+builder.Services.AddDbContext<TransactionDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<TransactionRepository>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
