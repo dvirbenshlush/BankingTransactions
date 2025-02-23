3cb8eadd769da42fbcbd5e8942cd1a498687f48b
dvirbenshlush <dvirbsh95@gmail.com>
2025-02-24 00:06:56 +0200

    add basic style and order code

--- BankTransactionsClient/src/app/components/transactions/transactions.component.ts
+++ BankTransactionsClient/src/app/components/transactions/transactions.component.ts
@@ -1,26 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { Transaction } from '../../models/transaction.model';
-import { selectTransactions } from '../../state/bank/transaction.selectors';
-import { TransactionActions } from '../../state/bank/transaction.actions';
+import { Store } from '@ngrx/store';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon'; 
 import { MatTableModule } from '@angular/material/table';
+import { Transaction } from '../../models/transaction.model';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TransactionActions } from '../../state/bank/transaction.actions';
+import { selectTransactions } from '../../state/bank/transaction.selectors';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, FormsModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.scss'],
 })
 export class TransactionsComponent implements OnInit {
   transactions$: Observable<ReadonlyArray<Transaction>> = this.store.select(selectTransactions);
-  displayedColumns: string[] = ['accountNumber', 'amount', 'status', 'externalTransactionId', 'identityNumber', 'transactionDate'];
+  displayedColumns: string[] = [
+    'accountNumber',
+    'amount',
+    'status',
+    'transactionDate',
+    'actions' // הוספת עמודת הפעולות
+  ];
+  // displayedColumns: string[] = ['accountNumber', 'amount', 'status', 'externalTransactionId', 'identityNumber', 'transactionDate'];
 
   constructor(private store: Store) {}
 
   ngOnInit() {
     this.store.dispatch(TransactionActions.loadTransactionHistory());
+  }
+
+  toggleEdit(transaction: any) {
+    transaction.isEditing = !transaction.isEditing;
+  }
+
+  editField(transaction: any) {
+    console.log(`Editing field: for transaction`, transaction);
+    // כאן תוכל להוסיף לוגיקה של שמירה על הערכים המועדים לשינוי
+  }
+
+  deleteField(transaction: any) {
+    console.log(`Deleting field: for transaction`, transaction);
+    // כאן תוכל להוסיף לוגיקה למחיקת ערך מהשדה או מהטרנזקציה
   }
 }
