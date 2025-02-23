c321d8d7d8eb9066fdf634fa37d739cb362850ef
dvirbenshlush <dvirbsh95@gmail.com>
2025-02-23 05:31:07 +0200

    order code and add form

--- BankTransactionsClient/src/app/app.component.ts
+++ BankTransactionsClient/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TransactionsComponent } from './components/transactions/transactions.component';
+import { TransactionFormComponent } from './components/transaction-form/transaction-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TransactionsComponent],
+  imports: [RouterOutlet, TransactionsComponent, TransactionFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- BankTransactionsClient/src/app/services/transaction.service.ts
+++ BankTransactionsClient/src/app/services/transaction.service.ts
@@ -2,14 +2,20 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Transaction } from "../models/transaction.model";
+import { TransactionRequest } from "../models/transaction-request.model";
+import { ApiResponse } from "../models/api-response.model";
 
 @Injectable({ providedIn: 'root' })
 export class TransactionService {
-  private apiUrl = 'https://localhost:7064/api/Transactions/GetAllTransactions';
+  private apiUrl = 'https://localhost:7064/api/';
 
   constructor(private http: HttpClient) {}
 
   getTransactionHistory(): Observable<Array<Transaction>> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>(this.apiUrl + 'Transactions/getAllTransactions');
+  }
+
+  createTransaction(req: TransactionRequest): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(this.apiUrl + 'Transactions/createTransaction', req);
   }
 }
